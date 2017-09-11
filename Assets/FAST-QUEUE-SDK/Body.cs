7ca85463171dcdb00d1dd98957299e2053da834a
boveloco <boveloco@gmail.com>
2017-09-11 16:09:55 -0300

    Initial commit

---
+++ Assets/FAST-QUEUE-SDK/Body.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UB.SimpleCS.Models;
+
+public class Body : EmptyDto {
+
+	public Body(string name){
+		this.name = name;
+	}
+
+	private string name;
+}
---
+++ Assets/FAST-QUEUE-SDK/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UB.SimpleCS;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+	public Text resText;
+
+	RestApi api = RestApi.Instance;
+	string url = "http://tcc-andre.ddns.net/queue";
+	string key = "minhavidaeandarporestepais";
+
+	void Start(){
+	}
+
+	public void getAllQueue(){
+		var d = api.Send<Body>(RequestType.Get, url, key, null);
+
+		resText.text = d;
+		Debug.Log (d);
+	}
+
+	public void addQueue(string b){
+		var d = api.Send<Body>(RequestType.Post, url, key, new Body(b));
+
+		resText.text = d;
+		Debug.Log (d);
+	}
+
+}
