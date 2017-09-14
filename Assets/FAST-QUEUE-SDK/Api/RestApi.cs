9656743bf1807454e96b111ead2a0d341affe3e6
boveloco <boveloco@gmail.com>
2017-09-14 15:40:53 -0300

    Delete Queue, Delete player usando 2 templates<T, K> Formatação

--- Assets/FAST-QUEUE-SDK/Api/RestApi.cs
+++ Assets/FAST-QUEUE-SDK/Api/RestApi.cs
@@ -3,137 +3,146 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using Newtonsoft.Json;
-using UnityEngine.Networking;
 using UnityEngine;
+using UnityEngine.Networking;
 
 namespace FQ {
-	
-    public class RestApi
-    {
-		private string key;
-		private string baseUrl;
 
-		public RestApi(){}
+    public class RestApi {
+        private string key;
+        private string baseUrl;
 
-		public RestApi(string url, string key)
-		{
-			this.baseUrl = url;
-			this.key = key;
+        public RestApi () { }
+
+        public RestApi (string url, string key) {
+            this.baseUrl = url;
+            this.key = key;
+        }
+        public T deleteQueue<T> (T queue) where T : FQ.BaseBody {
+            var url = baseUrl + "/queue/" + queue._id;
+            return this.delete<T> (url);
         }
 
-		public T addQueue<T>(T obj) where T: FQ.BaseBody {
-			var url = baseUrl + "/queue";
-			return add(url, obj);
-		}
+        public T deletePlayer<T, K> (T queue, K player) where T : FQ.BaseBody
+                                                        where K : FQ.BaseBody {
+            var url = baseUrl + "/queue/" + queue._id + "/players/" + player._id;
+            return this.delete<T> (url);
+        }
 
-		public T[] getQueue<T>() {
-			var url = baseUrl + "/queue";
-			var queues = get<T>(url);
-			if(queues.Length > 0)
-				return queues;
-			return null;
-		}
-		public T getQueue<T>(string id) {
-			var url = baseUrl + "/queue/" + id;
-			return getOne<T>(url);
-		}
+        public T addQueue<T> (T obj) where T : FQ.BaseBody {
+            var url = baseUrl + "/queue";
+            return add (url, obj);
+        }
 
-		public T[] getPlayers<T>(string _id) {
-			var url = baseUrl + "/queue/" + _id + "/players";
-			return get<T>(url);
-		}
+        public T[] getQueue<T> () {
+            var url = baseUrl + "/queue";
+            var queues = get<T> (url);
+            if (queues.Length > 0)
+                return queues;
+            return null;
+        }
+        public T getQueue<T> (string id) {
+            var url = baseUrl + "/queue/" + id;
+            return getOne<T> (url);
+        }
 
-		public T getPlayers<T>(string queueId, string playerId) {
-			var url = baseUrl + "/queue/" + queueId + "/players/" + playerId;
-			return getOne<T>(url);
-		}
+        public T[] getPlayers<T> (string _id) {
+            var url = baseUrl + "/queue/" + _id + "/players";
+            return get<T> (url);
+        }
 
-		public T addPlayer<T>(string _id, T obj) where T: FQ.BaseBody{
-			var url = baseUrl + "/queue/" + _id + "/players";
-			return add<T>(url, obj);
-		}
+        public T getPlayers<T> (string queueId, string playerId) {
+            var url = baseUrl + "/queue/" + queueId + "/players/" + playerId;
+            return getOne<T> (url);
+        }
 
-		private T add<T>(string url, T obj) where T: FQ.BaseBody {
-			T retObj = obj;
-			var type = RequestType.Post;
-			if(obj == null){
-				throw new Exception ("No object recived on post!");
-			}
-			var sendObj = objectToJSON(obj);
-			var request = this.Send (url, type, sendObj);
-			T response = convertPostResponse<T> (request);
-			retObj._id = response._id;
-			return retObj;
-		}
+        public T addPlayer<T> (string _id, T obj) where T : FQ.BaseBody {
+            var url = baseUrl + "/queue/" + _id + "/players";
+            return add<T> (url, obj);
+        }
 
-		private T[] get<T>(string url){
-			var type = RequestType.Get;
-			var request = this.Send (url, type, null);
-			var x = "{ objects:" + request + "}";
-			var response = convertPostResponse<getAllToWork<T>> (x);
-			return response.objects;
-		}
+        private T add<T> (string url, T obj) where T : FQ.BaseBody {
+            T retObj = obj;
+            var type = RequestType.Post;
+            if (obj == null) {
+                throw new Exception ("No object recived on post!");
+            }
+            var sendObj = objectToJSON (obj);
+            var request = this.Send (url, type, sendObj);
+            T response = convertResponse<T> (request);
+            retObj._id = response._id;
+            return retObj;
+        }
 
-		private T getOne<T>(string url){
-			var type = RequestType.Get;
-			var request = this.Send (url, type, null);
-			var response = convertPostResponse<T> (request);
-			return response;
-		}
+        private T delete<T> (string url) {
+            var type = RequestType.Delete;
+            var request = this.Send (url, type, null);
+            var response = convertResponse<T> (request);
+            return response;
+        }
 
-			
-		private string Send(string url, RequestType apiRequestType, string body)
-		{
-			var request = HttpWebRequest.Create(new System.Uri(url));
-			request.Headers ["API-KEY"] = this.key;
+        private T[] get<T> (string url) {
+            var type = RequestType.Get;
+            var request = this.Send (url, type, null);
+            var x = "{ objects:" + request + "}";
+            var response = convertResponse<getAllToWork<T>> (x);
+            return response.objects;
+        }
+
+        private T getOne<T> (string url) {
+            var type = RequestType.Get;
+            var request = this.Send (url, type, null);
+            var response = convertResponse<T> (request);
+            return response;
+        }
+
+        private string Send (string url, RequestType apiRequestType, string body) {
+            var request = HttpWebRequest.Create (new System.Uri (url));
+            request.Headers["API-KEY"] = this.key;
             request.Timeout = 21000; //milliseconds
-			if (apiRequestType == RequestType.Get) {
-				request.Method = "GET";
-			} else if (apiRequestType == RequestType.Post) {
-				request.Method = "POST";
-			} else if (apiRequestType == RequestType.Delete) {
-				request.Method = "DELETE";
-			} else if (apiRequestType == RequestType.Put) {
-				request.Method = "PUT";
-			}
-
-
-            if (body != null)
-            {
-                var dataToSend = Encoding.UTF8.GetBytes(body);
-                request.ContentType = "application/json";
-                request.ContentLength = dataToSend.Length;
-                request.GetRequestStream().Write(dataToSend, 0, dataToSend.Length);
+            if (apiRequestType == RequestType.Get) {
+                request.Method = "GET";
+            } else if (apiRequestType == RequestType.Post) {
+                request.Method = "POST";
+            } else if (apiRequestType == RequestType.Delete) {
+                request.Method = "DELETE";
+            } else if (apiRequestType == RequestType.Put) {
+                request.Method = "PUT";
             }
 
-            var response = request.GetResponse();
+            if (body != null) {
+                var dataToSend = Encoding.UTF8.GetBytes (body);
+                request.ContentType = "application/json";
+                request.ContentLength = dataToSend.Length;
+                request.GetRequestStream ().Write (dataToSend, 0, dataToSend.Length);
+            }
+
+            var response = request.GetResponse ();
 
             //HttpResponseMessage response = client.PostAsync(url, content).Result;
-            using (var reader = new System.IO.StreamReader(response.GetResponseStream(), Encoding.UTF8))
-            {
-                string responseString = reader.ReadToEnd();
-        
+            using (var reader = new System.IO.StreamReader (response.GetResponseStream (), Encoding.UTF8)) {
+                string responseString = reader.ReadToEnd ();
+
                 return responseString;
             }
         }
 
-		private T convertPostResponse<T>(string response){
-			//just trying to convert response to my helper empty dto, so i can understand if any error happened:)
-			var body = JsonConvert.DeserializeObject<T>(response);
-			if (body == null )
-			{
-				throw new Exception("Erro conversão json - POST"); //throw error message
-			}
-			return body;
-		}
+        private T convertResponse<T> (string response) {
+            //just trying to convert response to my helper empty dto, so i can understand if any error happened:)
+            var body = JsonConvert.DeserializeObject<T> (response);
+            if (body == null) {
+                throw new Exception ("Erro conversão json - POST"); //throw error message
+            }
+            return body;
+        }
 
-		private string objectToJSON(object obj){
-			var serialized = JsonConvert.SerializeObject(obj);
-			return serialized;
-		}
+        private string objectToJSON (object obj) {
+            var serialized = JsonConvert.SerializeObject (obj);
+            return serialized;
+        }
     }
-	class getAllToWork<T>{
-		
-		public T []objects;
-	}
+    class getAllToWork<T> {
+
+        public T[] objects;
+    }
 }
