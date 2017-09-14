2b9dfeba978431198fe223108209e26a63168988
boveloco <boveloco@gmail.com>
2017-09-14 14:42:11 -0300

    getters e setters e formatação de código.

--- Assets/FAST-QUEUE-SDK/Model/BaseBody.cs
+++ Assets/FAST-QUEUE-SDK/Model/BaseBody.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace FQ {
-	
-	public class BaseBody {
 
-		public BaseBody(){
-		}
-		public string _id;
+    public class BaseBody {
 
-		protected string toJson(){
-			return "{ _id: " + _id + " } "; 
-		}
-	}
+        public BaseBody () { }
+        public string _id;
 
+        public void setId (string id) {
+            this._id = id;
+        }
+
+        public string getId () {
+            return this._id;
+        }
+
+        protected string toJson () {
+            return "{ _id: " + _id + " } ";
+        }
+    }
 
 }
